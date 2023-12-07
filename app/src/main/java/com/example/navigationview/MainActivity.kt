59ae64b68e13f380af4f58ca77a07bea33693d94
Matheus <malmeidaarruda2@gmail.com>
2023-12-07 16:22:42 -0300

    Initial commit

--- app/src/main/java/com/example/navigationview/MainActivity.kt
+++ app/src/main/java/com/example/navigationview/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.navigationview
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
+
+    private val homeFragment = HomeFragment()
+    private val feedFragment = FeedFragment()
+    private val searchFragment = SearchFragment()
+    private val profileFragment = ProfileFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,26 +21,40 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
 
+        // Define o fragmento inicial
+        setCurrentFragment(homeFragment)
+
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> {
+                    setCurrentFragment(homeFragment)
                     showToast("Home")
                     true
                 }
                 R.id.nav_feed -> {
+                    setCurrentFragment(feedFragment)
                     showToast("Feed")
                     true
                 }
                 R.id.nav_search -> {
+                    setCurrentFragment(searchFragment)
                     showToast("Pesquisar")
                     true
                 }
                 R.id.nav_profile -> {
+                    setCurrentFragment(profileFragment)
                     showToast("Perfil")
                     true
                 }
                 else -> false
             }
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+            commit()
         }
     }
 
